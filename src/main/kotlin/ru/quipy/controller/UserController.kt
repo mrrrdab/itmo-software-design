8eaebd1cc979c16ea10e1c509639923aa63e7f04
djsjah <awesom-e4000@mail.ru>
2024-11-25 23:01:56 +0300

    add user projection

--- src/main/kotlin/ru/quipy/controller/UserController.kt
+++ src/main/kotlin/ru/quipy/controller/UserController.kt
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.*
 import ru.quipy.core.EventSourcingService
 import ru.quipy.api.user.*
 import ru.quipy.logic.user.*
+import ru.quipy.projections.user.UserViewDomain
+import ru.quipy.projections.user.interfaces.IUserViewRepository
 import java.util.*
 
 @RestController
 @RequestMapping("/users")
 class UserController(
-    val userEsService: EventSourcingService<UUID, UserAggregate, UserAggregateState>
+    val userEsService: EventSourcingService<UUID, UserAggregate, UserAggregateState>,
+    private val userRepository: IUserViewRepository
 ) {
 
   @PostMapping()
@@ -24,5 +27,15 @@ class UserController(
   @GetMapping("/{userId}")
   fun getUser(@PathVariable userId: UUID): UserAggregateState? {
     return userEsService.getState(userId)
+  }
+
+  @GetMapping("/nickname/{userNickname}")
+  fun findByNickname(@PathVariable userNickname: String): UserViewDomain? {
+    return userRepository.findByNickname(userNickname)
+  }
+
+  @GetMapping("/fullName/{userFullName}")
+  fun findByFullName(@PathVariable userFullName: String): List<UserViewDomain> {
+    return userRepository.findByFullName(userFullName)
   }
 }
